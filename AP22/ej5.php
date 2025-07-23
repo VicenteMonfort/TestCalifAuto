f34f4fb25ee8662898a09f3f23b689202669448c
VicenteMonfort <vmonfort@florida-uni.es>
2025-07-23 12:57:13 +0200

    Errores sintácticos

--- AP22/ej5.php
+++ AP22/ej5.php
@@ -1,14 +1,14 @@
 <?php
     
-    define("PI",3.14);
+    defines("PI",3.14);
     $h = $_GET["h"];
     $r = $_GET["r"];
   
 
-    $area1 = (PI * $r**2)/2 ;
+    area1 = (PI * $r**2)/2 ;
     $area2 = $r * $h; //no divido entre 2 porque no multiplico el radio por 2
 
-    $area = $area1 + $area2;
+    $area = $area1 + area2;
 
     echo "El area es: " .$area;
 
